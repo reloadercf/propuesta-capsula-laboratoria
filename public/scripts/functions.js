2a20ec9affb94d758704a27fad5e96875a96d37a
reloadercf <elcfm1@gmail.com>
2020-04-15 01:17:59 -0500

    fist upload with register

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -4,4 +4,15 @@ function logIn(email, password){
 
 function register(email, password){
     console.log('Register - Email: '+email+', Password: '+password);
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+        console.log(user)
+        let mail=user.user.email
+        alert(`${mail} se registro correctamente`)
+    }).catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // ...
+        alert(`${errorCode} ${errorMessage} no se puede`)
+      });
 }
